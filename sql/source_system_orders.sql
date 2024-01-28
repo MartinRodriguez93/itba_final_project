be0d42da7bd9fbedeb96f438072cd53565815ee5
Martín Rodríguez <rodriguez.martin2608@gmail.com>
2024-01-28 09:06:15 -0300

    Create source_system_orders.sql

---
+++ sql/source_system_orders.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS source_dataset.source_system_orders (
+Order_ID STRING,
+Order_Date STRING,
+Customer_ID STRING,
+Product_ID STRING,
+Quantity INTEGER,
+INGESTION_DATE STRING,
+UPDATE_TS TIMESTAMP
+)
