caf0e97d6982285cebacdad61504266252c61bbd
Martín Rodríguez <rodriguez.martin2608@gmail.com>
2024-02-01 19:05:08 -0300

    olist changes (#12)

---
+++ models/staging/olist/stg_olist__orders.sql
@@ -0,0 +1,25 @@
+with
+
+    source as (select * from {{ source("olist", "source_system_orders") }}),
+
+    stg_orders as (
+
+        select
+            order_id,
+            customer_id,
+            order_status,
+            TIMESTAMP (order_purchase_timestamp) AS order_purchase_ts,
+            TIMESTAMP (order_approved_at) as order_approved_at_ts,
+            TIMESTAMP (order_delivered_carrier_date) as order_delivered_carrier_ts,
+            TIMESTAMP (order_delivered_customer_date) as order_delivered_customer_ts,
+            TIMESTAMP (order_estimated_delivery_date) as order_estimated_delivery_ts,
+            DATE (INGESTION_DATE) as INGESTION_dt,
+            TIMESTAMP (UPDATE_TS) as UPDATE_TS
+
+        from source
+
+    )
+
+select *
+from stg_orders
+    
